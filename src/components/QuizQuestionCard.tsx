59a1a5e90d2172c0fb1c7e42deeee3f28f44a924
Michael Anthony Bitoon <mbitoon@up.edu.ph>
2026-09-13 05:15:50 +0800

    lower vibration

--- src/components/QuizQuestionCard.tsx
+++ src/components/QuizQuestionCard.tsx
@@ -33,7 +33,7 @@ export default function QuizQuestionCard({question, onOptionSelect, OnBack} : Qu
                     <ul className="option-list">
                         {question.options.map((o) => (
                                 <OptionListItem key ={o.label} option={o} onClick={()=>{
-                                    navigator.vibrate?.(15)
+                                    navigator.vibrate?.(8)
                                     setTimeout(() => onOptionSelect(o.label), 250)
                                 }}/>
                             ))}
